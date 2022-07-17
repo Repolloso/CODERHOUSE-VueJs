4c7f0953c3960de5c99237c6122afe5d19a5cd3d
Agustin <agustin@zylalabs.com>
2022-07-17 18:13:04 -0300

    tarea 6

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
+<!-- Where you construct your page adding the components -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <TableComponent 
+      :msg="msg"
+      :estilo_verde="estilo_verde"
+      :estilo_amarillo="estilo_amarillo"
+      :estilo_rojo="estilo_rojo"
+      :blanco="blanco"
+      :Json="Json"
+    />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import the components that will be used in the app
+import TableComponent from './components/TableComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TableComponent,
+  },
+  data () {
+    return {
+      msg: 'Tabla con Vue CLI',
+      estilo_verde: 'background-color: green; color: white;',
+      estilo_amarillo: 'background-color: yellow;',
+      estilo_rojo: 'background-color: red;',
+      blanco: 'color: white;',
+      Json: [
+              {
+                  cantidad: 12,
+                  producto: 'Leche',
+                  precio: 200
+              },
+              {
+                  cantidad: 21,
+                  producto: 'Carne',
+                  precio: 351
+              },
+              {
+                  cantidad: 43,
+                  producto: 'Arroz',
+                  precio: 512
+              },
+              {
+                  cantidad: 54,
+                  producto: 'Chocolate',
+                  precio: 631
+              },
+              {
+                  cantidad: 52,
+                  producto: 'Cafe',
+                  precio: 52
+              },
+              {
+                  cantidad: 6,
+                  producto: 'Salchichas',
+                  precio: 63
+              },
+        ],
+    }
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-image: linear-gradient(to right, #1768e1, #5a62ed, #8a57f3, #b743f2, #e112eb);
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+    display: flex;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
 }
 </style>
---
+++ src/components/TableComponent.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <section class="table-component">
+    
+    <h1 class="pb-3">{{msg}}</h1>
+
+    <div class="container">
+      <table class="table table-fluid">
+          <thead>
+              <tr>
+                  <th scope="col">Cantidad</th>
+                  <th scope="col">Producto</th>
+                  <th scope="col">Precio</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="(product) in Json" :key="product.id">
+                  <td>{{product.cantidad}}</td>
+                  <td>{{product.producto}}</td>
+                  <td v-if="product.precio < 100" :style="estilo_verde">{{product.precio}}</td>
+                  <td v-else-if="product.precio >=100 && product.precio <=351" :style="estilo_amarillo" class="text-dark">{{product.precio}}</td>
+                  <td v-else :style="estilo_rojo">{{product.precio}}</td>
+              </tr>
+          </tbody>
+      </table>
+    </div>
+
+  </section>
+</template>
+
+<script>
+  export default  {
+    name: 'table-component',
+    props: {
+      msg: String,
+      estilo_rojo: String,
+      estilo_amarillo: String,
+      estilo_verde: String,
+      blanco: String,
+      Json: Array
+    },
+    
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+        
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: #fff !important;
+    font-size: 2em;
+    font-weight: bold;
+    text-shadow: 0 0 10px black;
+  }
+  th {
+    background-color: #42b983;
+    color: #fff;
+    text-shadow: 0 0 30px black;
+  }
+  td {
+    color: #fff;
+  }
+</style>
